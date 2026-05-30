797ca312869daf5d6439b4a397c6234882cca933
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-05-29 22:03:59 -0300

    Creating component pagination

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,6 +5,7 @@ import searchSvg from "../assets/search.svg";
 import { RefundItem } from "../components/RefundItem";
 import { CATEGORIES } from "../utils/categories";
 import { FormatCurrency } from "../utils/FormatCurrency";
+import Pagination from "../components/Pagination";
 
 const REFUND_EXEMPLE = {
   id: "123",
@@ -16,10 +17,25 @@ const REFUND_EXEMPLE = {
 
 export function Dashboard() {
   const [name, setName] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalOfPages, setTotalOfPages] = useState(10);
 
   function fetchRedunds(e: React.SubmitEvent) {
     e.preventDefault();
     console.log(name);
+  }
+
+  function handlePagination(action: "next" | "previous") {
+    setPage((prevPage) => {
+      if (action === "next" && prevPage < totalOfPages) {
+        return prevPage + 1;
+      }
+      if (action === "previous" && prevPage > 1) {
+        return prevPage - 1;
+      }
+
+      return prevPage;
+    });
   }
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-3xl ">
@@ -42,6 +58,13 @@ export function Dashboard() {
       <div className="mt-6 flex flex-col gap-4 max-h-85.5 overflow-y-scroll">
         <RefundItem data={REFUND_EXEMPLE} />
       </div>
+
+      <Pagination
+        current={page}
+        total={totalOfPages}
+        onNext={() => handlePagination("next")}
+        onPrevious={() => handlePagination("previous")}
+      />
     </div>
   );
 }
