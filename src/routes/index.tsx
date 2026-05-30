fae1b3d83caf66ea7022074ba9c9eaee3e221f8d
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-05-29 22:44:56 -0300

    Adjust routes according to user roles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,38 @@
 import { BrowserRouter } from "react-router-dom";
 
 // import { AuthRoutes } from "./Auth-routes";
-//import { EmployeeRoutes } from "./Employee-rouths";
+// import { EmployeeRoutes } from "./Employee-rouths";
 import { ManagerRoutes } from "./Manager-routes";
+import { Loading } from "../components/Loading";
+import { EmployeeRoutes } from "./Employee-rouths";
+import { AuthRoutes } from "./Auth-routes";
+
+const isLoading = false;
+// const session = undefined;
+const session = {
+  user: {
+    role: "",
+  },
+};
+
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
